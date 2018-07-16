39a63fec427d64b7381caf87d175843d81888173
Nick Amin <amin.nj@gmail.com>
2018-07-16 08:05:17 -0700

    update conversion scripts

--- Analysis/new/convert.py
+++ Analysis/new/convert.py
@@ -37,129 +37,144 @@ def clamp(arr, lower=None, upper=None):
         arr[arr > upper] = upper
     return arr
 
-def convert_file(fname, outputdir="outputs/", prefix="", suffix="", treename="tree"):
+def convert_file(fname, outputdir="outputs_29x15_tt_v1/", prefix="", suffix="", treename="tree"):
     if not os.path.exists(fname):
         raise Exception("File does not exist: {}".format(fname))
 
     if not os.path.exists(outputdir):
         os.system("mkdir -p {}".format(outputdir))
 
-    f = uproot.open(fname)
-    t = f[treename]
+    try:
+        f = uproot.open(fname)
+        t = f[treename]
 
-    ele_oldsigmaietaieta_, ele_oldsigmaiphiiphi_, \
-        ele_oldcircularity_, ele_oldr9_, \
-        ele_scletawidth_, ele_sclphiwidth_, \
-        ele_kfhits_, ele_oldhe_, \
-        ele_psEoverEraw_, ele_kfchi2_, \
-        ele_chi2_hits_, ele_fbrem_, \
-        ele_ep_, ele_eelepout_, \
-        ele_IoEmIop_, ele_deltaetain_, \
-        ele_deltaphiin_, ele_deltaetaseed_, \
-        ele_gsfhits_, ele_expectedMissingInnerHits_, \
-        ele_convVtxFitProbability_, \
-        mvas, seed_ietas, seed_iphis, seed_es, \
-        charges, rhs_iphis, rhs_ietas, rhs_es, \
-        pts, etas, truenumints, theircode = \
-        t.arrays([
-                "ele_oldsigmaietaieta", "ele_oldsigmaiphiiphi",
-                "ele_oldcircularity", "ele_oldr9",
-                "ele_scletawidth", "ele_sclphiwidth",
-                "ele_kfchi2", "ele_oldhe",
-                "ele_psEoverEraw", "ele_kfchi2",
-                "ele_gsfchi2", "ele_fbrem",
-                "ele_ep", "ele_eelepout",
-                "ele_IoEmIop", "ele_deltaetain",
-                "ele_deltaphiin", "ele_deltaetaseed",
-                "ele_gsfhits", "ele_expected_inner_hits",
-                "ele_conversionVertexFitProbability",
-                "ele_ID1", "seed_ieta", "seed_iphi",
-                "seed_e", "ele_echarge", "rhs_iphi",
-                "rhs_ieta", "rhs_e", "ele_pt",
-                "ele_eta", "MC_TrueNumInteractions",
-                "mc_ele_matchedFromCB",
-                ], outputtype=tuple)
+        ele_oldsigmaietaieta_, ele_oldsigmaiphiiphi_, \
+            ele_oldcircularity_, ele_oldr9_, \
+            ele_scletawidth_, ele_sclphiwidth_, \
+            ele_kfhits_, ele_oldhe_, \
+            ele_psEoverEraw_, ele_kfchi2_, \
+            ele_chi2_hits_, ele_fbrem_, \
+            ele_ep_, ele_eelepout_, \
+            ele_IoEmIop_, ele_deltaetain_, \
+            ele_deltaphiin_, ele_deltaetaseed_, \
+            ele_gsfhits_, ele_expectedMissingInnerHits_, \
+            ele_convVtxFitProbability_, \
+            mvas, seed_ietas, seed_iphis, seed_es, \
+            charges, rhs_iphis, rhs_ietas, rhs_es, \
+            pts, etas, truenumints, theircode, \
+            ele_isEBEtaGap, ele_isEBPhiGap, \
+            ele_isEBEEGap, ele_isEEDeeGap, ele_isEERingGap = \
+            t.arrays([
+                    "ele_oldsigmaietaieta", "ele_oldsigmaiphiiphi",
+                    "ele_oldcircularity", "ele_oldr9",
+                    "ele_scletawidth", "ele_sclphiwidth",
+                    "ele_kfchi2", "ele_oldhe",
+                    "ele_psEoverEraw", "ele_kfchi2",
+                    "ele_gsfchi2", "ele_fbrem",
+                    "ele_ep", "ele_eelepout",
+                    "ele_IoEmIop", "ele_deltaetain",
+                    "ele_deltaphiin", "ele_deltaetaseed",
+                    "ele_gsfhits", "ele_expected_inner_hits",
+                    "ele_conversionVertexFitProbability",
+                    "ele_ID1", "seed_ieta", "seed_iphi",
+                    "seed_e", "ele_echarge", "rhs_iphi",
+                    "rhs_ieta", "rhs_e", "ele_pt",
+                    "ele_eta", "MC_TrueNumInteractions",
+                    "mc_ele_matchedFromCB",
+                    "ele_isEBEtaGap", "ele_isEBPhiGap",
+                    "ele_isEBEEGap", "ele_isEEDeeGap", "ele_isEERingGap",
+                    ], outputtype=tuple)
 
-    # 0, 1, 3 = bkg unmatch, sig, bkg from b/c
-    matchtype = np.copy(theircode)
-    # remap from their convention to mine
-    matchtype[matchtype == 0] = 0
-    matchtype[matchtype == 1] = 2
-    matchtype[matchtype == 3] = 1
+        # 0, 1, 3 = bkg unmatch, sig, bkg from b/c
+        matchtype = np.copy(theircode)
+        # remap from their convention to mine
+        matchtype[matchtype == 0] = 0
+        matchtype[matchtype == 1] = 2
+        matchtype[matchtype == 3] = 1
 
-    ele_deltaetain_ = np.abs(ele_deltaetain_)
-    ele_deltaphiin_ = np.abs(ele_deltaphiin_)
-    ele_deltaetaseed_ = np.abs(ele_deltaetaseed_)
-    ele_oldcircularity_ = clamp(ele_oldcircularity_, lower=-1, upper=2.)
-    ele_oldr9_ = clamp(ele_oldr9_, upper=5.)
-    ele_kfchi2_ = clamp(ele_kfchi2_, upper=10.)
-    ele_chi2_hits_ = clamp(ele_chi2_hits_, upper=200.)
-    ele_fbrem_ = clamp(ele_fbrem_, lower=-1.)
-    ele_ep_ = clamp(ele_ep_, upper=20.)
-    ele_eelepout_ = clamp(ele_eelepout_, upper=20.)
-    ele_deltaetain_ = clamp(ele_deltaetain_, upper=0.06)
-    ele_deltaphiin_ = clamp(ele_deltaphiin_, upper=0.6)
-    ele_deltaetaseed_ = clamp(ele_deltaetaseed_, upper=0.2)
-    ele_oldsigmaiphiiphi_[np.abs(ele_oldsigmaiphiiphi_) > 1e9] = 0.
+        ele_deltaetain_ = np.abs(ele_deltaetain_)
+        ele_deltaphiin_ = np.abs(ele_deltaphiin_)
+        ele_deltaetaseed_ = np.abs(ele_deltaetaseed_)
+        ele_oldcircularity_ = clamp(ele_oldcircularity_, lower=-1, upper=2.)
+        ele_oldr9_ = clamp(ele_oldr9_, upper=5.)
+        ele_kfchi2_ = clamp(ele_kfchi2_, upper=10.)
+        ele_chi2_hits_ = clamp(ele_chi2_hits_, upper=200.)
+        ele_fbrem_ = clamp(ele_fbrem_, lower=-1.)
+        ele_ep_ = clamp(ele_ep_, upper=20.)
+        ele_eelepout_ = clamp(ele_eelepout_, upper=20.)
+        ele_deltaetain_ = clamp(ele_deltaetain_, upper=0.06)
+        ele_deltaphiin_ = clamp(ele_deltaphiin_, upper=0.6)
+        ele_deltaetaseed_ = clamp(ele_deltaetaseed_, upper=0.2)
+        ele_oldsigmaiphiiphi_[np.abs(ele_oldsigmaiphiiphi_) > 1e9] = 0.
 
-    mvamat = np.c_[
-            ele_oldsigmaietaieta_, ele_oldsigmaiphiiphi_,
-            ele_oldcircularity_, ele_oldr9_,
-            ele_scletawidth_, ele_sclphiwidth_,
-            ele_kfhits_, ele_oldhe_,
-            ele_psEoverEraw_, ele_kfchi2_,
-            ele_chi2_hits_, ele_fbrem_,
-            ele_ep_, ele_eelepout_,
-            ele_IoEmIop_, ele_deltaetain_,
-            ele_deltaphiin_, ele_deltaetaseed_,
-            ele_gsfhits_, ele_expectedMissingInnerHits_,
-            ele_convVtxFitProbability_,
-            ]
+        mvamat = np.c_[
+                ele_oldsigmaietaieta_, ele_oldsigmaiphiiphi_,
+                ele_oldcircularity_, ele_oldr9_,
+                ele_scletawidth_, ele_sclphiwidth_,
+                ele_kfhits_, ele_oldhe_,
+                ele_psEoverEraw_, ele_kfchi2_,
+                ele_chi2_hits_, ele_fbrem_,
+                ele_ep_, ele_eelepout_,
+                ele_IoEmIop_, ele_deltaetain_,
+                ele_deltaphiin_, ele_deltaetaseed_,
+                ele_gsfhits_, ele_expectedMissingInnerHits_,
+                ele_convVtxFitProbability_,
+                ]
 
-    isgood = (pts > 5.) & (pts < 2.0e3) & (seed_es > 1.e-6) & (np.abs(etas) < 1.4)
+        isgood = (pts > 5.) & (pts < 2.0e3) & (seed_es > 1.e-6) & (np.abs(etas) < 1.4)
 
-    images = []
-    for good, rhs_e, rhs_iphi, rhs_ieta, rhs_e, \
-            seed_e, seed_iphi, seed_ieta, charge \
-            in zip(isgood, rhs_es, rhs_iphis, rhs_ietas, rhs_es,
-                   seed_es, seed_iphis, seed_ietas, charges):
+        images = []
+        for good, rhs_e, rhs_iphi, rhs_ieta, rhs_e, \
+                seed_e, seed_iphi, seed_ieta, charge \
+                in zip(isgood, rhs_es, rhs_iphis, rhs_ietas, rhs_es,
+                       seed_es, seed_iphis, seed_ietas, charges):
 
-        if not good: continue
+            if not good: continue
 
-        image = get_image(
-                rhs_iphi,rhs_ieta,rhs_e,
-                seed_iphi, seed_ieta,
-                charge
-                )
-        images.append(image)
+            image = get_image(
+                    rhs_iphi,rhs_ieta,rhs_e,
+                    seed_iphi, seed_ieta,
+                    charge
+                    )
+            images.append(image)
 
-    x_data = np.array(images)
-    mva_data = mvamat[isgood]
-    y_data = np.c_[
-            matchtype,
-            pts,
-            etas,
-            mvas,
-            seed_iphis,
-            seed_ietas,
-            truenumints,
-            ][isgood]
+        x_data = np.array(images)
+        mva_data = mvamat[isgood]
+        y_data = np.c_[
+                matchtype,
+                pts,
+                etas,
+                mvas,
+                seed_iphis,
+                seed_ietas,
+                truenumints,
+                ele_isEBEtaGap,
+                ele_isEBPhiGap,
+                ele_isEBEEGap,
+                ele_isEEDeeGap,
+                ele_isEERingGap,
+                ][isgood]
 
-    idx = int(fname.rsplit(".",1)[0].split("_")[-1])
-    outfname = "{}/{}data{}_{}.npz".format(outputdir,prefix,suffix,idx)
-    np.savez_compressed(outfname,
-                        mva_data=np.array(mva_data,dtype=np.float32),
-                        x_data=np.array(x_data,dtype=np.float32),
-                        y_data=np.array(y_data,dtype=np.float32),
-                        )
+        idx = int(fname.rsplit(".",1)[0].split("_")[-1])
+        outfname = "{}/{}data{}_{}.npz".format(outputdir,prefix,suffix,idx)
+        np.savez_compressed(outfname,
+                            mva_data=np.array(mva_data,dtype=np.float32),
+                            x_data=np.array(x_data,dtype=np.float32),
+                            y_data=np.array(y_data,dtype=np.float32),
+                            )
 
-    return len(x_data)
+        return len(x_data)
+
+    except:
+        pass
 
 if __name__ == "__main__":
 
     pool = ThreadPool(10)
-    thedir = "/hadoop/cms/store/user/namin/elemva/ProjectMetis/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2_MINIAODSIM_v10/"
-    fnames = glob.glob(thedir + "/*.root")
+    # thedir = "/hadoop/cms/store/user/namin/elemva/ProjectMetis/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2_MINIAODSIM_v10/"
+    # thedir = "/hadoop/cms/store/user/namin/elemva/ProjectMetis/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2_MINIAODSIM_v13/"
+    thedir = "/hadoop/cms/store/user/namin/elemva/ProjectMetis/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1_MINIAODSIM_v15/"
+    fnames = glob.glob(thedir + "/*.root")[:40]
     for N in tqdm(pool.imap_unordered(convert_file,fnames),total=len(fnames)):
         pass
         # print ">>> Processed {} events (so far)".format(N)
--- Analysis/new/utils.py
+++ Analysis/new/utils.py
@@ -1,13 +1,21 @@
+import time
+import os
 import glob
 import numpy as np
+from sklearn.metrics import roc_curve
 
-def load_data(inputdir="outputs/", nfiles=-1, nevents=-1, skipfiles=0):
+def load_data(inputdir="outputs/", nfiles=-1, nevents=-1, skipfiles=0,selector=None,return_track_info=False,return_time_info=False):
     """
     Load data from `inputdir`, up to a maximum of `nfiles` files OR
     `nevents` events (whichever comes first), skipping the first
     `skipfiles` files
+    `selector` is an optional lambda function applied to y_data_ returning an array
+    of bools to only store selected x,y,mva data
     """
     list_mva_data = []
+    list_track_data = []
+    list_time_data = []
+    list_chi2_data = []
     list_x_data = []
     list_y_data = []
     tot_events = 0
@@ -20,25 +28,61 @@ def load_data(inputdir="outputs/", nfiles=-1, nevents=-1, skipfiles=0):
         x_data_ = data["x_data"]
         y_data_ = data["y_data"]
         mva_data_ = data["mva_data"]
+        if return_track_info:
+            track_data_ = data["track_data"]
+        if return_time_info:
+            time_data_ = data["time_data"]
+            chi2_data_ = data["chi2_data"]
+        if selector:
+            sel = selector(y_data_)
+            x_data_ = x_data_[sel]
+            y_data_ = y_data_[sel]
+            mva_data_ = mva_data_[sel]
+            if return_track_info:
+                track_data_ = track_data_[sel]
+            if return_time_info:
+                time_data_ = time_data_[sel]
+                chi2_data_ = chi2_data_[sel]
         list_mva_data.append(mva_data_)
         list_x_data.append(x_data_)
         list_y_data.append(y_data_)
+        if return_track_info:
+            list_track_data.append(track_data_)
+        if return_time_info:
+            list_time_data.append(time_data_)
+            list_chi2_data.append(chi2_data_)
         tot_events += len(y_data_)
         nfilesopen += 1
         if nfiles > 0 and nfilesopen >= nfiles:
             break
         if nevents > 0 and tot_events >= nevents:
             break
-        print("Loaded {}".format(fname))
+        print("Loaded {} ({} events total)".format(fname, tot_events))
     mva_data = np.concatenate(list_mva_data)
     x_data = np.concatenate(list_x_data)
     y_data = np.concatenate(list_y_data)
+    if return_track_info:
+        track_data = np.concatenate(list_track_data)
+    if return_time_info:
+        time_data = np.concatenate(list_time_data)
+        chi2_data = np.concatenate(list_chi2_data)
     if nevents > 0:
         mva_data = mva_data[:int(nevents)]
         x_data = x_data[:int(nevents)]
         y_data = y_data[:int(nevents)]
+        if return_track_info:
+            track_data = track_data[:int(nevents)]
+        if return_time_info:
+            time_data = time_data[:int(nevents)]
+            chi2_data = chi2_data[:int(nevents)]
     print("Loaded {} files with {} events".format(nfilesopen,len(y_data)))
-    return x_data, y_data, mva_data
+    if return_track_info:
+        if return_time_info:
+            return x_data, y_data, mva_data, track_data, time_data, chi2_data
+        else:
+            return x_data, y_data, mva_data, track_data
+    else:
+        return x_data, y_data, mva_data
 
 def train_test_split(*args,**kwargs):
     train_size = 1.-kwargs.get("test_size", 0.5)
@@ -66,3 +110,52 @@ def culled_indices(thresholds, precision=0.0002):
         else:
             continue
     return np.array(to_keep)
+
+def sigeff_for_given_bkgeff(y_true, y_pred, bkgeff, sel=None):
+    """
+    For a given background efficiency, this returns the
+    signal efficiency, with an optional selection on the input vectors
+    """
+    yt,yp = y_true, y_pred
+    if sel is not None:
+        yt = yt[sel]
+        yp = yp[sel]
+    fpr, tpr, _ = roc_curve(yt,yp)
+    idx = np.where(fpr > bkgeff)[0][0]
+    return tpr[idx]
+
+def get_sigeffs_for_bins(y_true,y_pred,selector,bins,bkgeff):
+    """
+    Takes true class labels, predictions, a list of bin edges, and
+    a given background efficiency. For each bin (of the selector), computes the
+    signal efficiency for that background efficiency. Returns a 3-column matrix
+    with columns [bin center, signal efficiency, weight], where weight
+    is sqrt(number of samples in bin)
+    """
+    to_ret = []
+    for low,high in zip(bins[:-1],bins[1:]):
+        sel = ((selector > low) & (selector < high))
+        nsamps = sel.sum()
+        if nsamps <= 10: continue
+        sigeff = sigeff_for_given_bkgeff(y_true, y_pred, bkgeff=bkgeff, sel=sel)
+        to_ret.append( [0.5*(low+high), sigeff, np.sqrt(nsamps)] )
+    return np.array(to_ret)
+
+def get_pteta_weights(pts,etas,is_signal,ptbins=None,etabins=None):
+    """
+    Takes arrays of pts, etas, is_signal bools, and optional pt/eta bins
+    and returns an array of weights matching pts.shape with 1 for signal,
+    reweighting bkg to signal
+    """
+    if ptbins is None:
+        ptbins = np.array(range(5,78,2)+range(80,200,5)+range(200,300,10)+range(300,500,100)+range(500,1000+1,500)+[4000])
+    if etabins is None:
+        etabins = np.linspace(-1.4,1.4,20)
+    counts_sig, _,_ = np.histogram2d(pts[is_signal],etas[is_signal],bins=[ptbins,etabins])
+    counts_bkg, _,_ = np.histogram2d(pts[~is_signal],etas[~is_signal],bins=[ptbins,etabins])
+    ptidxs = np.digitize(pts[~is_signal],ptbins)-1
+    etaidxs = np.digitize(etas[~is_signal],etabins)-1
+    weights = np.ones(len(pts))
+    weights[~is_signal] = (1.0*counts_sig[ptidxs,etaidxs]/counts_bkg[ptidxs,etaidxs])
+    return weights
+
